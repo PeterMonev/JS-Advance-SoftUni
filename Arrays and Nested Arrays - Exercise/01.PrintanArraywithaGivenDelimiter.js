0ee3559ad680b707949ca75eae84f6210dcb6323
PeterMonev <monevpeter@gmail.com>
2022-12-01 09:22:02 +0200

    Solving a task

---
+++ Arrays and Nested Arrays - Exercise/01.PrintanArraywithaGivenDelimiter.js
@@ -0,0 +1,6 @@
+function printAnArrayWithAGivenDelimeter(array,delimeter) {
+    'use strict';
+    console.log(array.join(delimeter));
+
+}
+printAnArrayWithAGivenDelimeter(["One", "Two", "Three", "Four", "Five"], "-");
