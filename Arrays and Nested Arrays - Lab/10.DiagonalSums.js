8130a74b442a76dd4ff3c3011706130eb60ba8b4
PeterMonev <monevpeter@gmail.com>
2022-12-07 09:37:41 +0200

    Solving a task

---
+++ Arrays and Nested Arrays - Lab/10.DiagonalSums.js
@@ -0,0 +1,22 @@
+function diagonalSums(matrix) {
+  'use strict';
+  let firstDiagonalSum = 0;
+  let secondDiagonalSum = 0;
+
+  for(let i = 0; i < matrix.length ; i++) {
+      firstDiagonalSum += matrix[i][i];
+      secondDiagonalSum += matrix[i][matrix.length - 1 - i]
+  }
+
+console.log(firstDiagonalSum+' '+secondDiagonalSum);
+    
+}
+diagonalSums([
+  [20, 40],
+  [10, 60],
+]);
+diagonalSums([
+  [3, 5, 17],
+  [-1, 7, 14],
+  [1, -8, 89],
+]);
