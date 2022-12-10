d2f507c9893ef25cfdc6dc161df9fb35e95a3f8f
PeterMonev <monevpeter@gmail.com>
2022-12-10 08:00:54 +0200

    Solving a task

---
+++ Classes and Attributes - Exercise/10. Contacts-Builder/app.js
@@ -0,0 +1,16 @@
+class Contact {
+    constructor(firstName, lastName,phone,email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email
+    }
+}
+
+let contacts = [
+    new Contact("Ivan", "Ivanov", "0888 123 456", "i.ivanov@gmail.com"),
+    new Contact("Maria", "Petrova", "0899 987 654", "mar4eto@abv.bg"),
+    new Contact("Jordan", "Kirov", "0988 456 789", "jordk@gmail.com")
+  ];
+  contacts.forEach(c => c.render('main'));
+  
