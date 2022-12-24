b76ddb037668f8c6c1f4cfebe3b17cfab8841ee5
PeterMonev <monevpeter@gmail.com>
2022-12-24 09:08:04 +0200

    Solving a task

---
+++ DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,13 @@
+function calc() {
+
+    const num1 = Number(document.getElementById('num1').value);
+    const num2 = Number(document.getElementById('num2').value);
+    const sum = num1 + num2;
+
+    if(Number.isNaN(sum)){
+        document.getElementById('sum').value = "Error! Insert Number"
+     } else {
+        document.getElementById('sum').value = sum;
+    }
+
+}
