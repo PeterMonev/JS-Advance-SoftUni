293b8804833de34f05cb0194fe91e6080ad8e5e3
PeterMonev <monevpeter@gmail.com>
2023-02-03 09:11:03 +0200

    Solving a task

---
+++ Objects and Composition - Lab/01.CityRecord.js
@@ -0,0 +1,12 @@
+function cityRecord(name,population,treasury) {
+    'use strict';
+    
+    const obj = {
+        name: name,
+        population: population,
+        treasury: treasury
+    };
+
+    return obj;
+}
+cityRecord("Tortuga", 7000, 15000);
