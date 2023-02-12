c17e9f08f167d96424304098c03c1d608db25b1a
PeterMonev <monevpeter@gmail.com>
2023-02-12 11:14:30 +0200

    Solving a task

---
+++ Syntax, Functions and Statements - Lab/02.StringLength.js
@@ -0,0 +1,7 @@
+function stringLenght(arr1,arr2,arr3){
+   const sum = arr1.length + arr2.length + arr3.length;
+   console.log(sum);
+   console.log( Math.trunc(sum / 3))
+
+}
+stringLenght('chocolate', 'ice cream', 'cake');
